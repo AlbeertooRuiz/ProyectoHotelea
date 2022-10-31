b7b8db8a082c56d708b37c58d2f4dd7865d72db4
alber <alber@DESKTOP-LK68B99>
2022-10-31 19:10:23 +0100

    BD

---
+++ ProyectoHotelea/src/hotelea/HotelBD.java
@@ -0,0 +1,48 @@
+package hotelea;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HotelBD {
+	public static Connection initBD(String nombreBD) {
+		Connection con = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:"+nombreBD);
+					
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public static void closeBD(Connection con) {
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void crearTablas(Connection con) {
+		String sql = "CREATE TABLE IF NOT EXISTS Hotel (Nombre String, Estrellas String, Ciudad String)";
+		try {
+			Statement st = con.createStatement();
+			st.executeUpdate(sql);
+			st.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
