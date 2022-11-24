b68988fe8690e457e46a80a06da3b47c34470972
jonayo <jonayo@localhost>
2022-11-24 11:17:40 +0100

    obtenerdatoshotel y creacion tabla hoteles

--- ProyectoHotelea/src/Datos/Hotel.java
+++ ProyectoHotelea/src/Datos/Hotel.java
@@ -1,6 +1,7 @@
 package Datos;
 
 public class Hotel {
+	private String nombre;
 	private String codH;	//codigo del hotel
 	private String dir;		//direccion del hotel
 	private String ciudad;	//ciudad en la que se ubica el hotel
@@ -12,14 +13,22 @@ public class Hotel {
 		super();
 	}
 
-	public Hotel(String codH, String dir, String ciudad, int numEstr, String tlfn, int numHab) {
+	public Hotel(String nombre,String codH, String dir, String ciudad, int numEstr, String tlfn, int numHab) {
 		super();
+		this.nombre=nombre;
 		this.codH = codH;
 		this.dir = dir;
 		this.ciudad = ciudad;
 		this.numEstr = numEstr;
 		this.tlfn = tlfn;
 		this.numHab = numHab;
+	}
+	
+	public Hotel(String nombre, int numEstr, String ciudad) {
+		super();
+		this.nombre=nombre;
+		this.numEstr = numEstr;
+		this.ciudad = ciudad;
 	}
 
 	public String getCodH() {
@@ -69,10 +78,19 @@ public class Hotel {
 	public void setNumHab(int numHab) {
 		this.numHab = numHab;
 	}
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 	@Override
 	public String toString() {
-		return "Hotel [codH=" + codH + ", dir=" + dir + ", ciudad=" + ciudad + ", numEstr=" + numEstr + ", tlfn=" + tlfn
+		return "Hotel [nombre=" + nombre + ",codH=" + codH + ", dir=" + dir + ", ciudad=" + ciudad + ", numEstr=" + numEstr + ", tlfn=" + tlfn
 				+ ", numHab=" + numHab + "]";
 	}
 }
