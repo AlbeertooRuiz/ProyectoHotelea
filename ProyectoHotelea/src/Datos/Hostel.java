7f4b4826077ed76aaccdc16b47f4fceb3d7669d7
alber <alber@PC-Alberto>
2023-01-22 17:03:12 +0100

    Prueba

--- ProyectoHotelea/src/Datos/Hostel.java
+++ ProyectoHotelea/src/Datos/Hostel.java
@@ -4,10 +4,12 @@ public class Hostel extends Hotel{
 	
 	private int camasPorHab;
 	
+	//Constructor vacio de Hostel
 	public Hostel() {
 		super();
 	}
 	
+	//Constructor de Hostel
 	public Hostel(String nombre, String codH, String dir, String ciudad, int estrellas, String tlfn, int numHab,
 			int precio, int valoracion,int camasPorHab) {
 		super();
